3eb92eaa0b7eaa7f6195f1ed09ff99b5c4e36297
Summaw <98126132+Summaw@users.noreply.github.com>
2022-12-25 20:22:11 -0500

    Update server.py

--- examples/server.py
+++ examples/server.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, jsonify, make_response
 from captcha_detector import Captcha_detection
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return Captcha_detection('12.png')
+    response = {
+        "Answer": Captcha_detection('12.png'),
+        "Solved": True,
+    }
+    return make_response(jsonify(response), 200)
+    # print(Captcha_detection('12.png'))
 
 if __name__ == '__main__':
     app.run()
