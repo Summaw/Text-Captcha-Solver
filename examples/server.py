2e6ac4f816281df2e64c0f26af71b101fec21853
Summaw <98126132+Summaw@users.noreply.github.com>
2023-11-07 18:39:42 -0500

    Updated server.py
    Downloads photo using requests rather then selenium 👍 💯

--- examples/server.py
+++ examples/server.py
@@ -1,13 +1,9 @@
 from flask import Flask, jsonify, make_response, request
 from captcha_detector import Captcha_detection
-from selenium import webdriver
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.by import By
 from time import sleep
-from webdriver_manager.chrome import ChromeDriverManager
 import time
 import urllib
+import httpx
 
 app = Flask(__name__)
 
@@ -15,29 +11,25 @@ app = Flask(__name__)
 def hello_world():
     new_url = request.args.get('url')
     print(request.args.get('url'))
-    options = webdriver.ChromeOptions()
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-    options.add_argument("--disable-web-security")
-    options.add_argument("--log-level=3")
-    options.add_argument('--disable-blink-features=AutomationControlled')
-    options.add_argument('--start-maximized')
-    options.add_experimental_option(
-        "excludeSwitches", ["enable-automation", "enable-logging"])
-    options.add_experimental_option('useAutomationExtension', False)
-    driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
-    driver.get(new_url)
-    m = driver.find_element(By.XPATH, '/html/body/img')
-    m.screenshot("captcha.png")
-    driver.close()
-    time.sleep(2)
+
+    session = httpx.Client(headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36'},timeout=30)
+    url = new_url
+    output_file = 'captcha.png'
+    response = session.get(url)
+
+    if response.status_code == 200:
+        with open(output_file, 'wb') as file:
+            file.write(response.content)
+
+        print(f"Photo downloaded and saved as '{output_file}'")
+    else:
+        print(f"Failed to download the photo. Status code: {response.status_code}")
 
     response = {
         "Solved": True,
         "response": Captcha_detection('captcha.png')
         }
     return make_response(jsonify(response), 200)
-    # print(Captcha_detection('12.png'))
 
 if __name__ == '__main__':
     app.run()
